f8eaeac1c0d0770d60a056f957c30e4e1efb22f7
Rafael L. <118671591+rafaellopesss@users.noreply.github.com>
2025-01-22 18:41:26 -0300

    att

---
+++ cosmetico/javascript/javascript.js
@@ -0,0 +1,16 @@
+let menu = document.getElementById('menu')
+let menu_icone= document.getElementById('menu-icone')
+
+let show = () => {
+    if (menu_icone.style.display == 'none'){
+        
+        menu.innerText = 'close'
+        menu_icone.style.display = 'block'
+
+
+    }else{
+        menu.innerText = 'menu'
+        menu_icone.style.display = 'none'
+    }
+    
+}
