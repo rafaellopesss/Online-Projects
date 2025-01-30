b09f586532f7858eb4098090e697710c7a6b4b22
Rafael L. <118671591+rafaellopesss@users.noreply.github.com>
2025-01-30 17:35:35 -0300

    sistema javascript

--- pedra-papel-tesoura/src/javascript.js
+++ pedra-papel-tesoura/src/javascript.js
@@ -1,21 +1,40 @@
 //Capturar a jogada do jogador
 //Criar uma lista com as possbilidades do computador
 // Comparar os dois itens acima
-let jogadascomputador = ['pedra', 'papel', 'tesoura']
 
 let jogada_computador =  () => {
-    
+    let jogadascomputador = ['pedra', 'papel', 'tesoura']
     let random = Math.floor(Math.random() * jogadascomputador.length)
-    console.log(jogadascomputador[random])
+    return (jogadascomputador[random]) // Eu escolho uma jogada aleatória da lusta
 }
 
+let jogadorpontos = 0 
+let robopontos = 0
 
 let jogada_do_jogador = () => {
-    let input_check = document.querySelector('input[name="jogada"]:checked')
 
-    if (input_check != undefined){
-        alert(jogada_computador())
-        
+    let jogadorJogada= document.querySelector('input[name="jogada"]:checked')
+    let jogadaComputador = jogada_computador()
+
+
+    if (jogadorJogada.value != undefined){
+        if (jogadorJogada.value == jogadaComputador) {
+            alert('Empate') //3 possibilidades
+            
+        }else if (jogadorJogada.value == 'pedra' && jogadaComputador == 'tesoura' || jogadorJogada.value == 'papel' && jogadaComputador == 'pedra' || jogadorJogada.value == 'tesoura' && jogadaComputador == 'papel'){ // 
+
+            alert('Você ganhou!')
+            alert(`Você jogou: ${jogadorJogada.value} / Computador Jogou: ${jogadaComputador}`)
+
+
+        }else {
+            alert('VOCÊ PERDEU!')
+            alert(`Você jogou: ${jogadorJogada.value} / Computador Jogou: ${jogadaComputador}`)
+
+        }
+
+            
+
 
     }else {
         alert('vazio')
@@ -25,6 +44,6 @@ let jogada_do_jogador = () => {
 
 
 let jogar = () => {
-    //jogada_do_jogador()
-    jogada_computador()
+    jogada_do_jogador()
+    
 }
