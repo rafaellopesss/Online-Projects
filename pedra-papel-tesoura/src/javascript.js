ef756d59b7a6fafe93a256471c16ed40c479a68d
Rafael L. <118671591+rafaellopesss@users.noreply.github.com>
2025-02-01 17:50:59 -0300

    atualizar

--- pedra-papel-tesoura/src/javascript.js
+++ pedra-papel-tesoura/src/javascript.js
@@ -14,24 +14,43 @@ let robopontos = 0
 let jogada_do_jogador = () => {
 
     let jogadorJogada= document.querySelector('input[name="jogada"]:checked')
+    let pontorobo = document.getElementById('maquina')
+    let seuspontos = document.getElementById('voce')
     let jogadaComputador = jogada_computador()
 
-
+    
     if (jogadorJogada.value != undefined){
-        if (jogadorJogada.value == jogadaComputador) {
-            alert('Empate') //3 possibilidades
+        if (jogadorpontos == 5 || robopontos == 5){
+            jogadorpontos = 0
+            robopontos = 0
+            pontorobo.innerText = robopontos
+            seuspontos.innerText = jogadorpontos
+
             
-        }else if (jogadorJogada.value == 'pedra' && jogadaComputador == 'tesoura' || jogadorJogada.value == 'papel' && jogadaComputador == 'pedra' || jogadorJogada.value == 'tesoura' && jogadaComputador == 'papel'){ // 
-
-            alert('Você ganhou!')
-            alert(`Você jogou: ${jogadorJogada.value} / Computador Jogou: ${jogadaComputador}`)
-
-
         }else {
-            alert('VOCÊ PERDEU!')
-            alert(`Você jogou: ${jogadorJogada.value} / Computador Jogou: ${jogadaComputador}`)
+            changeimage(jogadaComputador, jogadorJogada.value)
 
-        }
+            if (jogadorJogada.value == jogadaComputador) {
+                console.log('empate') //3 possibilidades
+                
+            }else if (jogadorJogada.value == 'pedra' && jogadaComputador == 'tesoura' || jogadorJogada.value == 'papel' && jogadaComputador == 'pedra' || jogadorJogada.value == 'tesoura' && jogadaComputador == 'papel'){ // 
+
+                
+                jogadorpontos ++
+            
+                seuspontos.innerText = jogadorpontos
+                
+
+
+            }else {
+                robopontos ++
+                
+                pontorobo.innerText = robopontos
+                
+
+            }
+        }    
+        
 
             
 
@@ -39,6 +58,16 @@ let jogada_do_jogador = () => {
     }else {
         alert('vazio')
     }
+    
+}
+
+let changeimage = (imagemcomputador, imagemyou) => { // Altera a imagem
+    let jogador_image = document.getElementById('jogadorimagem')
+    let computador_image = document.getElementById('computadorimagem')
+    
+    jogador_image.src = `imagens/${imagemyou}.png`
+    computador_image.src = `imagens/${imagemcomputador}.png` 
+       
     
 }
 
